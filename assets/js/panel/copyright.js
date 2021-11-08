c8228a0cf0e1117ebf45f2da3a728c7a20ed2cd1
Fruity <penguinttskm@gmail.com>
2021-11-08 11:19:26 +0000

    Update copyright.js

--- assets/js/panel/copyright.js
+++ assets/js/panel/copyright.js
@@ -1,9 +1,3 @@
-/**
- * @file copyright.js
- * @author Sanjay Sunil
- * @license GPL-3.0
- */
-
-$('.000').replaceWith('Copyright © 2021');
-$('.001').replaceWith('Sanjay Sunil');
-$('.002').replaceWith('All rights reserved.');
+$('.000').replaceWith('Hai');
+$('.001').replaceWith('Fruity');
+$('.002').replaceWith('');
